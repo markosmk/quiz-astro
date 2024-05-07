47bae0ded8611e31f54b28bb6ab850ba0578530b
markosmk <markosmk@me.com>
2024-05-07 14:51:40 -0300

    add: selected quiz to context

--- src/hooks/context.tsx
+++ src/hooks/context.tsx
@@ -4,17 +4,30 @@ import { createContext, useContext, useEffect, useState } from 'react';
 type Context = {
   quizzes: Quiz[];
   setQuizzes: React.Dispatch<React.SetStateAction<Quiz[]>>;
+  initQuiz: { selectedQuiz: Quiz | null; setSelectedQuiz: React.Dispatch<React.SetStateAction<Quiz | null>> };
 };
 
 const AppContext = createContext<Context>({} as Context);
 
 export function ContextProvider({ children }: { children: React.ReactNode }) {
   const [quizzes, setQuizzes] = useState<Quiz[]>([]);
+  const [selectedQuiz, setSelectedQuiz] = useState<Quiz | null>(null);
 
   useEffect(() => {
     setQuizzes(quizData);
   });
-  return <AppContext.Provider value={{ quizzes, setQuizzes }}>{children}</AppContext.Provider>;
+
+  return (
+    <AppContext.Provider
+      value={{
+        quizzes,
+        setQuizzes,
+        initQuiz: { selectedQuiz, setSelectedQuiz },
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 }
 
 export default function useAppContext() {
