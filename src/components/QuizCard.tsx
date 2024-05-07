018e05cc943b5cca33c74116f72bdb371f150845
markosmk <markosmk@me.com>
2024-05-07 18:58:10 -0300

    use: useNavigate to handle link

--- src/components/QuizCard.tsx
+++ src/components/QuizCard.tsx
@@ -1,20 +1,27 @@
 import { EllipsisIcon, MedalIcon, PlayIcon } from 'lucide-react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import type { Quiz } from '@/data/quizData';
 import useAppContext from '@/hooks/context';
 import { calculateSuccessRate } from '@/lib/helpers';
 
 export function QuizCard(quiz: Quiz) {
+  const navigate = useNavigate();
   const { title, id, questions, icon } = quiz;
   const {
     initQuiz: { setSelectedQuiz },
   } = useAppContext();
   const globalSuccessRate = calculateSuccessRate(questions);
 
+  const handleClick = () => {
+    setSelectedQuiz(quiz);
+    console.log('setSelectedQuiz', quiz);
+    navigate('quiz/' + id);
+  };
+
   return (
     <li className="rounded-lg flex flex-col border border-gray-300 bg-white">
-      <Link to={'quiz/' + id} className="flex flex-col gap-2 p-4">
+      <div onClick={handleClick} className="flex flex-col gap-2 p-4 cursor-pointer">
         <div className="relative bg-black w-full h-32 flex justify-center items-center rounded-md">
           <div className="absolute cursor-pointer top-3 right-3">
             <EllipsisIcon className="w-6 h-6" color="white" />
@@ -29,14 +36,11 @@ export function QuizCard(quiz: Quiz) {
             <MedalIcon className="w-4 h-4" color="green" />
             <span className="text-xs">Success rate: {globalSuccessRate}%</span>
           </div>
-          <div
-            className="rounded-full bg-black w-6 h-6 flex items-center justify-center cursor-pointer"
-            onClick={() => setSelectedQuiz(quiz)}
-          >
+          <div className="rounded-full bg-black w-6 h-6 flex items-center justify-center">
             <PlayIcon className="w-4 h-4" color="white" />
           </div>
         </div>
-      </Link>
+      </div>
     </li>
   );
 }
