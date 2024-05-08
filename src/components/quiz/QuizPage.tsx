403817b2399a3962eb32e8bcfcde89d9ad65b04e
markosmk <markosmk@me.com>
2024-05-08 00:34:26 -0300

    fix: reset running timer

--- src/components/quiz/QuizPage.tsx
+++ src/components/quiz/QuizPage.tsx
@@ -1,5 +1,6 @@
 import { useAppContext } from '@/hooks/context';
 import { useTimer } from '@/hooks/useTimer';
+import { Link } from 'react-router-dom';
 import { QuizContent } from './QuizContent';
 import { QuizHeader } from './QuizHeader';
 
@@ -10,11 +11,11 @@ export function QuizPage() {
 
   const { seconds, timeElapsed, reStart, stop } = useTimer({
     timeInSeconds: 25,
-    initiallyRunning: true,
+    initiallyRunning: selectedQuiz ? true : false,
   });
 
   return (
-    <div className="flex flex-col px-24 mt-10">
+    <div className="flex flex-col px-4 md:px-24 mt-10">
       {selectedQuiz ? (
         <>
           <QuizHeader
@@ -26,10 +27,13 @@ export function QuizPage() {
           <QuizContent seconds={seconds} startTimer={reStart} stopTimer={stop} />
         </>
       ) : (
-        <>
+        <div className="flex flex-col gap-2">
           <h2 className="text-3xl font-bold text-center">Please select a quiz</h2>
-          <p>No quiz selected</p>
-        </>
+          <p className="text-center">No quiz selected</p>
+          <Link to="/" className="p-3 px-4 text-white text-sm text-center bg-black rounded-md mt-4">
+            Go back
+          </Link>
+        </div>
       )}
     </div>
   );
